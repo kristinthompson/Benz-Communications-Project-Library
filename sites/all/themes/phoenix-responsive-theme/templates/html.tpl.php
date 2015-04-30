d7c5e5f2726d4d4ee2065e0e401b69699c27828c
kristinthompson <kristin@benzcommunications.com>
2015-04-30 16:33:19 -0500

    change accordion path so it works on dev and not local

--- sites/all/themes/phoenix-responsive-theme/templates/html.tpl.php
+++ sites/all/themes/phoenix-responsive-theme/templates/html.tpl.php
@@ -5,7 +5,7 @@
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script src="/website/sites/all/themes/phoenix-responsive-theme/js/accordion.js"></script>
+<script src="/sites/all/themes/phoenix-responsive-theme/js/accordion.js"></script>
 <!-- Add icons for mobile bookmarks -->
 <link href="http://http://dev-benz-communications-project-library.pantheon.io/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
 <link href="http://http://dev-benz-communications-project-library.pantheon.io/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
